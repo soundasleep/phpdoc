5f01853664e66b0b39633c0ba90645d6731fa08a
Jevon Wright <jevon@jevon.org>
2015-04-02 15:51:26 +1300

    Add initial tests for component

---
+++ test/PHPDocParserTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace PHPDocParser\Test;
+
+class PHPDocParserComponentTest extends \ComponentTests\ComponentTest {
+
+  function getRoots() {
+    return array(__DIR__ . "/..");
+  }
+
+}
