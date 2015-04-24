2be1409c7f6ff8851d92e1fd7d6a6bd159812408
Jevon Wright <jevon@jevon.org>
2015-04-24 13:09:23 +1200

    Refactor #findMethod()/#findClasslike() call in doc generation
    This allows both @see and @throws (and any future tags that may reference
    a method) to automatically get this functionality.

--- templates/_doc.php
+++ templates/_doc.php
@@ -44,21 +44,7 @@ if ($doc_reference->getInheritedDoc($this->logger, 'throws')) {
   echo "<dt>Throws:</dt>";
   echo "<dd>";
   foreach ($doc_reference->getInheritedDoc($this->logger, 'throws') as $thrown_class => $description) {
-    // try fqn
-    $discovered_class = $database->findClasslike($thrown_class, $this->logger);
-    if (!$discovered_class) {
-      // try our local namespace
-      $discovered_class = $database->findClasslike($doc_reference->getNamespace()->getName() . "\\" . $thrown_class, $this->logger);
-    }
-
-    if ($discovered_class) {
-      echo $this->linkTo($discovered_class->getFilename(), $discovered_class->getName());
-    } else {
-      echo $thrown_class;
-    }
-    if ($description) {
-      echo " - " . $this->formatInline($doc_reference, $description);
-    }
+    require(__DIR__ . "/_doc_hash.php");
     echo "<br>";
   }
   echo "</dd>";
@@ -67,43 +53,9 @@ if ($doc_reference->getInheritedDoc($this->logger, 'throws')) {
 if ($doc_reference->getInheritedDoc($this->logger, 'see')) {
   echo "<dt>See Also:</dt>";
   echo "<dd>";
+
   foreach ($doc_reference->getInheritedDoc($this->logger, 'see') as $see_class => $description) {
-    // try fqn
-    // e.g. @see Namespace\Class
-    $discovered_class = $database->findClasslike($see_class, $this->logger);
-    if (!$discovered_class) {
-      // try our local namespace
-      // e.g. @see Class
-      $discovered_class = $database->findClasslike($doc_reference->getNamespace()->getName() . "\\" . $see_class, $this->logger);
-    }
-
-    if (!$discovered_class) {
-      // try method search
-      // e.g. @see Namespace\Class#method
-      $discovered_method = $database->findMethod($see_class, $this->logger);
-      if (!$discovered_method) {
-        // try our local namespace
-        // e.g. @see Class#method
-        $discovered_method = $database->findMethod($doc_reference->getNamespace()->getName() . "\\" . $see_class, $this->logger);
-      }
-      if (!$discovered_method) {
-        // try our local namespace + Class
-        // e.g. @see #method
-        $discovered_method = $database->findMethod($doc_reference->getNamespace()->getName() . "\\" . $doc_reference->getClass()->getName() . $see_class, $this->logger);
-      }
-    }
-
-    if ($discovered_class) {
-      echo $this->linkTo($discovered_class->getFilename(), $discovered_class->getPrintableName());
-    } else if ($discovered_method) {
-      echo $this->linkTo($discovered_method->getFilename(), $discovered_method->getPrintableName());
-    } else {
-      echo $see_class;
-    }
-
-    if ($description) {
-      echo " - " . $this->formatInline($doc_reference, $description);
-    }
+    require(__DIR__ . "/_doc_hash.php");
     echo "<br>";
   }
   echo "</dd>";
